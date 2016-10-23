b3dbafc702a81345a1de06c983057dfe16a9b6c0
webee.yw <webee.yw@gmail.com>
2016-10-23 23:03:38 +0800

    change extra constants from c to C.

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ class RNXRPC {
   _xrpcSub: Object;
 
   constructor() {
-    this.c = XRPC;
+    this.C = XRPC.C;
     this._procedures = {};
     this._subscribers = {};
     this._xrpcSub = NativeAppEventEmitter.addListener(
