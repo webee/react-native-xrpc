4bbda773939a0863575f2327e0cb1017eabf9ba5
webee.yw <webee.yw@gmail.com>
2016-11-08 18:39:31 +0800

    add RNUtils for android.

---
+++ android/src/main/java/com/webee/react/RNUtils.java
@@ -0,0 +1,11 @@
+package com.webee.react;
+
+/**
+ * Created by webee on 16/11/8.
+ */
+
+public final class RNUtils {
+    public static Double Long(Long l) {
+        return l != null ? new Double(l) : null;
+    }
+}
